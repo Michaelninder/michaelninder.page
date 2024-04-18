2b35b125cf46487ab3a907572b080bebc2a938ba
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-04-18 22:10:21 +0200

    Update script.js

--- template/myfolio/script.js
+++ template/myfolio/script.js
@@ -21,4 +21,22 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
+
+    // Zeige den "hoch" Button, wenn der Benutzer nach unten scrollt
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    window.addEventListener("scroll", function() {
+        if (window.pageYOffset > 300) {
+            scrollToTopBtn.classList.add("show");
+        } else {
+            scrollToTopBtn.classList.remove("show");
+        }
+    });
+
+    // Scrollen nach oben, wenn der "hoch" Button geklickt wird
+    scrollToTopBtn.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
 });
